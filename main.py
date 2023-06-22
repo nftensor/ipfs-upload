a14a6d53e2d0b274cd504cb90ccf564fb3ab13d6
0xcacti <0xcacti@gmail.com>
2023-06-21 22:13:41 -0400

    fix read and write issue for modifying files

--- main.py
+++ main.py
@@ -7,11 +7,12 @@ from pinatapy import PinataPy
 IMAGE_DIR_PATH="./imgs/out"
 METADATA_DIR_PATH="./json/"
 
+UPLOAD_METADATA = False
+
 # handle upload to ipfs
 def upload_images(pinata):
 
     image_dir_path = os.path.abspath(IMAGE_DIR_PATH)
-    return
     pinata_response = pinata.pin_file_to_ipfs(image_dir_path, save_absolute_paths=False)
     return pinata_response["IpfsHash"]
 
@@ -21,15 +22,24 @@ def modify_metadata(ipfs_hash):
         # only operate on copies of the images to avoid any issue with corruption
         metadata_path = os.path.abspath(f"{METADATA_DIR_PATH}{i}")
         if os.path.exists(metadata_path):
-            with open(metadata_path, "rw") as f:
+            with open(metadata_path, "r") as f:
                 metadata = json.load(f)
-                metadata["image"] = f"ipfs://{ipfs_hash}/{i}.png"
-        print(metadata_path)
+             # Modify the 'image' field
+             # For example, let's set it to 'new_image.png'
+            if 'image' in metadata:
+                 metadata['image'] = f"ipfs://{ipfs_hash}/{i}.png"
+            else:
+                 print("'image' field not found in the JSON file")
+
+           # Save the modified JSON back to the file
+            with open(metadata_path, 'w') as file:
+                json.dump(metadata, file, indent=4)
+
+
 
 
 def upload_metadata(pinata):
     metadata_dir_path = os.path.abspath(METADATA_DIR_PATH)
-    return
     pinata_response = pinata.pin_file_to_ipfs(metadata_dir_path, save_absolute_paths=False)
     return pinata_response["IpfsHash"]
 
@@ -49,18 +59,19 @@ def main():
     # create pinata instance 
     pinata = PinataPy(pinata_api_key=api_key, pinata_secret_api_key=secret_key)
 
-
     # execute uplaoding and modification process
-    print("beginning image upload process")
-    ipfs_hash = upload_images(pinata)
-    print("image upload process complete")
 
+    if not UPLOAD_METADATA:
+        print("beginning image upload process")
+        ipfs_hash = upload_images(pinata)
+        print("image upload process complete")
+        print("beginning modification process")
+        modify_metadata(ipfs_hash)
+        print("modification process complete")
 
-    print("beginning modification process")
-    modify_metadata(ipfs_hash)
-    print("modification process complete")
-    print("beginning metadata upload process")
-    upload_metadata(pinata)
+    else: 
+        print("beginning metadata upload process")  
+        upload_metadata(pinata)
 
 
 if __name__ == "__main__":
