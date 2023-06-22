801e5406f32df3813d0b65e407d8d6372f8e5ba9
0xcacti <0xcacti@gmail.com>
2023-06-21 21:49:55 -0400

    code written, requires test

--- main.py
+++ main.py
@@ -1,40 +1,66 @@
 import dotenv
+import json
 import os 
 from pinatapy import PinataPy
 
+# make local copies of img and json directories here
 IMAGE_DIR_PATH="./imgs/out"
 METADATA_DIR_PATH="./json/"
 
-    # handle upload to ipfs
-def upload_images():
+# handle upload to ipfs
+def upload_images(pinata):
+
+    image_dir_path = os.path.abspath(IMAGE_DIR_PATH)
+    return
+    pinata_response = pinata.pin_file_to_ipfs(image_dir_path, save_absolute_paths=False)
+    return pinata_response["IpfsHash"]
+
+# handle modification of metadata
+def modify_metadata(ipfs_hash):
+    for i in range(1, 501):
+        # only operate on copies of the images to avoid any issue with corruption
+        metadata_path = os.path.abspath(f"{METADATA_DIR_PATH}{i}")
+        if os.path.exists(metadata_path):
+            with open(metadata_path, "rw") as f:
+                metadata = json.load(f)
+                metadata["image"] = f"ipfs://{ipfs_hash}/{i}.png"
+        print(metadata_path)
+
+
+def upload_metadata(pinata):
+    metadata_dir_path = os.path.abspath(METADATA_DIR_PATH)
+    return
+    pinata_response = pinata.pin_file_to_ipfs(metadata_dir_path, save_absolute_paths=False)
+    return pinata_response["IpfsHash"]
+
+
+def main(): 
     # load pinata api keys
     dotenv.load_dotenv()
     api_key = os.getenv("PINATA_API_KEY")
-    secret = os.getenv("PINATA_SECRET_API_KEY")
+    secret_key = os.getenv("PINATA_SECRET_API_KEY")
 
     # open image to upload
     # create pinata instance and upload to pinata
-    if api_key is None or secret is None:
+    if api_key is None or secret_key is None:
         print("Pinata API key or secret key is not set")
         return
 
+    # create pinata instance 
+    pinata = PinataPy(pinata_api_key=api_key, pinata_secret_api_key=secret_key)
 
-    pinata = PinataPy(pinata_api_key=api_key, pinata_secret_api_key=secret)
-    pinata_response = pinata.pin_file_to_ipfs(IMAGE_DIR_PATH, save_absolute_paths=False)
 
-    # return ipfs hash
-    return pinata_response["IpfsHash"]
-
-def main(): 
+    # execute uplaoding and modification process
     print("beginning image upload process")
-    # only operate on copies of the images to avoid any issue with corruption
-    ipfs_hash = upload_images()
+    ipfs_hash = upload_images(pinata)
     print("image upload process complete")
-    print(f"ipfs hash: {ipfs_hash}")
+
+
     print("beginning modification process")
-
-    
-
+    modify_metadata(ipfs_hash)
+    print("modification process complete")
+    print("beginning metadata upload process")
+    upload_metadata(pinata)
 
 
 if __name__ == "__main__":
